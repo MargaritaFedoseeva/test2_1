e653d30fa3a6e4f2a70f42be236394a9f127f7d2
MargaritaFedoseeva <fmargoha@rambler.ru>
2018-10-05 19:25:07 +0300

    Используя generics сделала: Класс в котором метод printArray выводящий в консоль содержимое массива вне зависимости что в нем хранится( примеры для вывода int , double, String)
    Добавила метод возвращаемый максимальное значение из массива вне зависимости что в нем хранится ( int или double)

---
+++ src/MyClass.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class MyClass {
+    public static <E> void printArray(E[] elements) {
+        for (E element : elements) {
+            System.out.print(element+"; ");
+        }
+        System.out.println();
+    }
+
+    public static <E> void maxElementInArray(E[] elements) {
+        ArrayList<E> arrayList = new ArrayList<>();
+        for (E element : elements) arrayList.add(element);
+        System.out.println("Максимальный элемент в массиве: " + Collections.max(arrayList, null));
+    }
+
+    public static void main(String args[]) {
+        Integer[] intArray = {10, 20, 30, 40, 50};
+        Double[] doubleArray = {10.1, 10.2, 30.1, 30.2, 21.001};
+        Character[] charArray = {'J', 'A', 'V', 'A', 'T', 'P', 'O', 'I', 'N', 'T'};
+        System.out.println("Printing Integer Array");
+        printArray(intArray);
+        maxElementInArray(intArray);
+        System.out.println("Printing Double Array");
+        printArray(doubleArray);
+        maxElementInArray(doubleArray);
+        System.out.println("Printing Character Array");
+        printArray(charArray);
+        maxElementInArray(charArray);
+    }
+}
